c1f519eaf0bd9994a6d8ae8bdbdbab3c55f2049c
Michael Provost <mprovost129@gmail.com>
2026-01-29 13:22:54 -0500

    Fixed production database

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,12 +6,9 @@ ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 # Render production database settings (use environment variables provided by Render)
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql',
-		'NAME': os.environ.get('DB_NAME'),
-		'USER': os.environ.get('DB_USER'),
-		'PASSWORD': os.environ.get('DB_PASSWORD'),
-		'HOST': os.environ.get('DB_HOST'),
-		'PORT': os.environ.get('DB_PORT'),
-	}
-}
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=IS_RENDER,  # SSL on Render; allow non-SSL locally if you ever set DATABASE_URL locally
+        )
+    }
