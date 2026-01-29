b72ba0eceb50df629e318a8caaf172504b6abd95
Michael Provost <mprovost129@gmail.com>
2026-01-29 11:25:08 -0500

    Fixed allowed hosts

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,4 +1,4 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'homecraft3d.onrender.com', 'www.homecraft3d.com', 'homecraft3d.com']
